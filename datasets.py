fixed backpropagation.

datasets.py
@@ -8,29 +8,29 @@ from PIL import Image
 import os
 
 class ODIR5K(Dataset):
-    def __init__(self, mode, augmentations=None):
+    def __init__(self, mode, transform=None):
         super(ODIR5K, self).__init__()
         self.mode = mode
-        self.csv_path = 'labels/pytorch_fake%s.csv' % (self.mode)
-        self.df = pd.read_csv(self.csv_path)
-        self.augmentations = augmentations
+        self.csvfile = 'labels/eye_labels_%s.csv' % (self.mode)
+        self.df = pd.read_csv(self.csvfile)
+        self.transform = transform
     
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, index):
-        csv_line = self.df.iloc[index]
-        img_path = csv_line['path']
-        img_path = 'data/images/%s/%s' % (self.mode, img_path.split('/')[-1])
-        label = torch.tensor(int(csv_line['label']))
+        row = self.df.iloc[index]
+        imagefile = row['ID']
+        imagefile = 'data/images/%s/%s' % (self.mode, imagefile.split('/')[-1])
+        label = torch.FloatTensor([int(i) for i in row['N':'O']])
         
-        if os.path.exists(img_path):
-            image = Image.open(img_path).convert('RGB')
+        if os.path.exists(imagefile):
+            image = Image.open(imagefile).convert('RGB')
         else:
-            print('%s not found' % (img_path))
+            print('%s not found' % (imagefile))
         
-        if self.augmentations:
-            image = self.augmentations(image)
+        if self.transform:
+            image = self.transform(image)
         
         return image, label
 
@@ -44,6 +44,7 @@ if __name__ == '__main__':
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
     for image, label in dataloader:
+        print(image, label)
         image = image.view(3, 500, 500).permute(1, 2, 0)
         plt.imshow(image)
         plt.savefig('figure/input.png')

labels.py
@@ -10,9 +10,14 @@ def main():
     # Myopia 0.9923 0.9946 0.9938 0.9942
     # Others
 
+    df = pd.read_csv('labels/eye_labels_train.csv', sep=',')
+    print(df)
+    print(df.loc[:, 'N':'O'].sum(axis=0))
+
     book = pd.ExcelFile('data/labels/ODIR-5K_Training_Annotations(Updated)_V2.xlsx')
     df = book.parse(book.sheet_names[0], index_col=0)
     print(df)
+    print(df.loc[:, 'N':'O'].sum(axis=0))
 
     print('Age stats:', df['Patient Age'].describe().to_dict())
     print('Sex stats:', df.groupby('Patient Sex').size().to_dict())
@@ -22,7 +27,8 @@ def main():
         for keyword in row['Left-Diagnostic Keywords'].split('，'):
             for label, value in zip(df.columns[6:], row['N':]):
                 if value:
-                    print(keyword, label, value)
+                    #print(keyword, label, value)
+                    pass
             #keyword_dict[keyword] = keyword_dict.get(keyword, 0) + 1 
 
         '''

main.py
@@ -6,6 +6,8 @@ from torch.nn import functional as F
 import torchvision.transforms as transforms
 from sklearn import metrics
 from sklearn import preprocessing
+import argparse
+import timeit
 import cv2
 
 import datasets
@@ -26,7 +28,7 @@ def get_color_distortion(s=1.0):
     color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
     return color_distort
 
-def odir_metric(gt_data, pr_data):
+def metric(gt_data, pr_data):
     threshold = 0.5
     gt = gt_data.flatten()
     pr = pr_data.flatten()
@@ -37,7 +39,7 @@ def odir_metric(gt_data, pr_data):
     
     return final_score
 
-def data_load(batch_size=32):
+def data_load(args):
     INPUT_IMG = 224
 
     color_transform = [get_color_distortion(), RandomGaussianBlur()]
@@ -55,7 +57,7 @@ def data_load(batch_size=32):
 
     # init the dataset and augmentations
     train_dataset = datasets.ODIR5K('train', transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
 
     # build the test augmentations
     transform = transforms.Compose([
@@ -65,7 +67,7 @@ def data_load(batch_size=32):
         ])
 
     test_dataset = datasets.ODIR5K('test', transform)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
 
     return train_loader, test_loader
 
@@ -92,7 +94,7 @@ def test():
     print(auc)
     
     labels_onehot = preprocessing.OneHotEncoder(sparse=False).fit_transform(np.array(labels).reshape(len(labels), 1))
-    final_score = odir_metric(labels_onehot, prob_preds)
+    final_score = metric(labels_onehot, prob_preds)
     print(final_score)
     
     cm = metrics.confusion_matrix(labels, y_pred)
@@ -100,63 +102,63 @@ def test():
     plot_confusion_matrix(cm, target_names=target_names, auc=auc, normalize=False)
     print(cm)
 
-class Identity(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return x
-
-def main(epochs=20, classes=8, learning_rate=5e-5, freeze=False):
+def main(args):
     device = torch.device('cuda:1')
 
-    train_loader, test_loader = data_load()
+    train_loader, test_loader = data_load(args)
 
     net = torch.hub.load('facebookresearch/swav', 'resnet50')
+
+    num_features = net.fc.in_features
+    net.fc = nn.Sequential(
+            nn.Linear(num_features, args.classes),
+            nn.Sigmoid())
+
     net = net.to(device)
 
-    d_dim = net.fc.in_features
-    net.fc = Identity()
-        
-    if freeze:
-        for p in net.parameters():
-            p.requires_grad = False
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+    #optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
+    criterion = torch.nn.CrossEntropyLoss()
 
-    linear_clf = nn.Linear(d_dim, classes)
-    linear_clf = linear_clf.to(device)
-
-    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-
-    #y_true = []
-    #y_pred = []
     net.train()
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
+        start_time = timeit.default_timer()
+        y_true = torch.FloatTensor()
+        y_pred = torch.FloatTensor()
         train_loss = 0
         for index, (images, labels) in enumerate(train_loader, 1):
             images = images.to(device)
             labels = labels.to(device)
 
-            representations = net(images) #.detach()
-            logits = linear_clf(representations)
-            loss = F.cross_entropy(logits, labels)
+            outputs = net(images)
+            loss = criterion(outputs, labels)
             train_loss += loss.item()
 
-            probs = F.softmax(logits, dim=1)
-            #y_true.append(labels.detach().cpu().numpy())
-            #y_pred.append(probs.detach().cpu())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-            print('\repoch %2d batch %2d/%2d loss %5.3f' % (epoch+1, index, len(train_loader), train_loss / index), end='')
+            y_true = torch.cat((y_true, labels.cpu()))
+            y_pred = torch.cat((y_pred, outputs.detach().cpu()))
+
+            print('\repoch %2d/%2d batch %3d/%3d' % (epoch+1, args.epochs, index, len(train_loader)), end='')
+            print(' loss %6.4f' % (train_loss / index), end='')
+            print(' %6.3fsec' % (timeit.default_timer() - start_time), end='')
 
         print('')
-
-        '''
-        auc = metrics.roc_auc_score(y_true, y_pred, average='weighted', multi_class='ovo')
-        print(auc)
-
-        labels_onehot = preprocessing.OneHotEncoder(sparse=False).fit_transform(np.array(y_true).reshape(len(y_true), 1))
-        final_score = odir_metric(labels_onehot, y_pred)
-        print(final_score)
-        '''
+        aucs = [metrics.roc_auc_score(y_true[:, i], y_pred[:, i]) for i in range(args.classes)]
+        auc_classes = ' '.join(['%5.3f' % (aucs[i]) for i in range(args.classes)])
+        print('The average AUC is %5.3f (%s)' % (np.mean(aucs), auc_classes))
+        torch.save(net.state_dict(), 'model/checkpoint.pth')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=20, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--classes', default=8, type=int)
+    parser.add_argument('--lr', default=1e-4, type=float) # 5e-5
+    parser.add_argument('--momentum', default=0.9, type=float)
+    args = parser.parse_args()
+    print(vars(args))
+
+    main(args)