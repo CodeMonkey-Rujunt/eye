3c17c7164618fa7afef313ef82428163f4b514fa
taneishi <taneishi@users.noreply.github.com>
2022-04-05 13:35:36 +0900

    fixed expression format.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class RandomGaussianBlur(object):
 
 def get_color_distortion(s=1.0):
     # s is the strength of color distortion.
-    color_jitter = transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
     rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
     rnd_gray = transforms.RandomGrayscale(p=0.2)
     color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
